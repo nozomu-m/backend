e25935a7939a4244f7c9c1a6def6f35869ea2448
Nozomu Miuran <miuran@toyo.co.jp>
2018-08-13 15:04:36 +0900

    Add method.

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 # -*- coding: utf-8 -*-
 # 日本語を使う場合は絶対に必要
 
+import datetime
 # flaskなどの必要なライブラリをインポート
 import os
-import datetime
+import time
+
 from flask import Flask
 from flask import jsonify
+from flask_cors import CORS
 
 # 自分の名称を app という名前でインスタンス化
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/')
 def index():
     return jsonify({"status": "OK", "Date": datetime.datetime.now()})
+
+
+@app.route('/unixtime')
+def unixtime():
+    return jsonify({"status": "OK", "UnixTime": time.time()})
+
 
 if __name__ == '__main__':
     app.run()
